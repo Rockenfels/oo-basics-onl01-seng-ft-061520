a4af47e33edf299a835c8bab24f6e8a1730572b4
Ray Ockenfels <ockenfelsr@gmail.com>
2020-06-26 01:57:31 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,6 +1,11 @@
 # Make your shoe class here!
 class Shoe
-  attr_accessor :brand, :color, :size, :material, :condition
+  attr_accessor :color, :size, :material, :condition
+  attr_reader :brand
+  
+  def initialize(brand)
+    @brand = brand
+  end
   
   def cobble
     @condition = "New"
