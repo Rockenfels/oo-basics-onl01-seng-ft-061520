c317ccfbc13d6c4557b1d649595a1e158161ca54
Ray Ockenfels <ockenfelsr@gmail.com>
2020-06-26 01:45:29 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,12 +1,18 @@
 class Book
   
-  attr_accessor :title, :author, :page_count, :genre
-  
   def initialize(title, author, page_count, genre)
     @title = title
     @author = author
     @page = page_count
     @genre = genre
+  end
+  
+  def title(title)
+    @title = title
+  end
+  
+  def title
+    @title
   end
   
   def turn_page
