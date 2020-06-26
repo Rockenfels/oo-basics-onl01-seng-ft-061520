2c105ffe15d7b809b7df8882de491969100dc0b8
Ray Ockenfels <ockenfelsr@gmail.com>
2020-06-26 01:41:17 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,17 @@
-
+class Book
+  
+  attr_accessor :title, :author, :page_count, :genre
+  
+  def initialize(title, author, page_count, genre)
+    @title = title
+    @author = author
+    @page = page_count
+    @genre = genre
+  end
+  
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+  
+end
 
