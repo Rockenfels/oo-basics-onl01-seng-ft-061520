ad9cfbc22e405ee0a3bfa15e0db526cb3a9d2917
Ray Ockenfels <ockenfelsr@gmail.com>
2020-06-26 01:52:03 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,47 +1,16 @@
 class Book
-  
-  def initialize(title, author, page_count, genre)
-    @title = title
-    @author = author
-    @page_count = page_count
-    @genre = genre
-  end
-  
-  def title(title)
+  attr_accessor :author, :page_count, :genre
+ 
+  def initialize(title)
     @title = title
   end
-  
+ 
   def title
     @title
   end
-  
-  def author(author)
-    @author = author
-  end
-  
-  def author
-    @author
-  end
-  
-  def page_count(page_count)
-    @page_count = page_count
-  end
-  
-  def page_count
-    @page_count
-  end
-  
-  def genre(genre)
-    @genre = genre
-  end
-  
-  def genre
-    @genre
-  end
-  
+ 
   def turn_page
     puts "Flipping the page...wow, you read fast!"
   end
-  
+ 
 end
-
